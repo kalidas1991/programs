64395c672ac09f68a3bd0fe7a97abbff548e5cf3
encrypted009 <prashantgandhile123@gmail.com>
2022-04-26 21:35:28 +0530

    ADDED FibonacciSeries printFibonacciSeries METHOD

--- src/com/app/java/basics/Driver.java
+++ src/com/app/java/basics/Driver.java
@@ -1,9 +1,15 @@
 package com.app.java.basics;
 
 public class Driver {
+//    done FIBONACCI SERIES
+//    TODO SUMOFDIGIT
+
     public static void main(String[] args) {
         System.out.println(ReverseNumber.reverseANumber(1005));
         System.out.println(ReverseNumber.reverseANumber(-89));
         System.out.println(ReverseString.reverseAString("ABCD"));
+        System.out.println("---fibonacci");
+        FibonacciSeries.printFibonacciSeries(100);
+        System.out.println();
     }
 }
--- src/com/app/java/basics/FibonacciSeries.java
+++ src/com/app/java/basics/FibonacciSeries.java
@@ -20,4 +20,21 @@ public class FibonacciSeries {
 			System.out.print(result + " ");
 		}
 	}
+
+	public static void printFibonacciSeries(int n){
+		if(n==0)
+			System.out.println(n);
+		int first  = 0;
+		int second = 1;
+		int result ;
+		System.out.print(first+" "+second+" ");
+		for (int i = 0; i < n; i++) {
+
+			result=first+second;
+			first=second;
+			second=result;
+			System.out.print(result+" ");
+		}
+
+	}
 }
